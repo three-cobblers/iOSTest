e30459f75e9ef09dd3ff9f840ff707c1c6bd351a
three-cobblers <threecobblers.163.com>
2020-01-10 15:16:32 +0800

    添加分支

--- GitTrainningProject/GitTrainningProject/SceneDelegate.m
+++ GitTrainningProject/GitTrainningProject/SceneDelegate.m
@@ -12,6 +12,12 @@
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
     // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
+    
+    
 }
 
 
