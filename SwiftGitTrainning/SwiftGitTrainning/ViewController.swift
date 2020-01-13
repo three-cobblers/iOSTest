99b375ce7a834e9732dcf5a85469aa8c3df36ccc
three-cobblers <threecobblers.163.com>
2020-01-13 14:45:44 +0800

    add a new swift project

---
+++ SwiftGitTrainning/SwiftGitTrainning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftGitTrainning
+//
+//  Created by 王凯荣 on 2020/1/13.
+//  Copyright © 2020 王凯荣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
